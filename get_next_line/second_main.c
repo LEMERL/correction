400732bf5e6443045b5e1e9770522bc37b498df8
Merlin GRIMALDI <mgrimald@e2r12p2.42.fr>
2015-01-27 13:51:34 +0100

    ah

--- get_next_line/second_main.c
+++ get_next_line/second_main.c
@@ -6,7 +6,7 @@
 /*   By: scoudert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/08 11:21:03 by scoudert          #+#    #+#             */
-/*   Updated: 2015/01/27 13:44:27 by mgrimald         ###   ########.fr       */
+/*   Updated: 2015/01/27 13:45:32 by mgrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,15 @@ int		main()
 	int i;
 
 	i = 42;
-	int		file = open("text.txt", O_RDONLY);
 	while (i != 0)
 	{
 		s = NULL;
-		i = get_next_line(file, &s);
+		i = get_next_line(0, &s);
 		if (i > 0)
 		{
 			printf("%s\n", s);
 			free(s);
 		}
 	}
-	close(file);
 	return (i);
 }
